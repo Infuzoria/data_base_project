e5e5d5f7be8724c957a7af330dd993c81d53e319
shurochka <alexa.sazhaeva@gmail.com>
2023-09-15 02:59:42 +0300

    Создали функцию vacancies_get_info

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import requests
-COMPANY_NAMES = ['Diasoft', 'Softline', 'VK', 'Альфа-Банк', 'Гринатом', 'Лаборатория Касперского',
-                 'Лига Цифровой Экономики', 'Московская Биржа', 'РТУ МИРЭА', 'Яндекс']
+from math import ceil
+COMPANY_NAMES = ['Diasoft', 'Softline', 'SKYPRO', 'VK', 'Вебиум', 'Гринатом', 'Лаборатория Касперского',
+                 'Лига Цифровой Экономики', 'Московская Биржа', 'РТУ МИРЭА']
 
 
 def get_ids(company_names: list[str]) -> dict[str, str]:
@@ -32,3 +33,34 @@ def company_get_info(company_ids: dict[str, str]) -> list[dict]:
         companies_info.append(data)
 
     return companies_info
+
+
+def vacancies_get_info(company_ids: dict[str, str]) -> list[dict]:
+    """Функция перебирает значения из списка, по id находит информацию о вакансиях"""
+
+    vacancies_info = []
+    for company_id in company_ids.values():
+        amount = requests.get('https://api.hh.ru/vacancies', params={'employer_id': company_id}).json()['found']
+        pages = ceil(amount/50)
+
+        for page in range(pages):
+            vacancies = requests.get('https://api.hh.ru/vacancies',
+                                     params={'employer_id': company_id, 'page': page, 'per_page': 50}).json()['items']
+
+            for vacancy in vacancies:
+
+                try:
+                    data = {'id': vacancy['id'], 'name': vacancy['name'], 'company_id': company_id,
+                            'salary_from': vacancy['salary']['from'], 'salary_to': vacancy['salary']['to'],
+                            'currency': vacancy['currency'], 'url': vacancy['alternate_url']}
+                except TypeError as e:
+                    data = {'id': vacancy['id'], 'name': vacancy['name'], 'company_id': company_id,
+                            'salary_from': None, 'salary_to': None, 'currency': None, 'url': vacancy['alternate_url']}
+                except KeyError as e:
+                    if e.args[0] == 'currency':
+                        data = {'id': vacancy['id'], 'name': vacancy['name'], 'company_id': company_id,
+                                'salary_from': vacancy['salary']['from'], 'salary_to': vacancy['salary']['to'],
+                                'currency': None, 'url': vacancy['alternate_url']}
+                vacancies_info.append(data)
+
+    return vacancies_info
